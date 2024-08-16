71189eab2a1969fc7668579398744286193350a8
Yusun16 <yusunguairabryan@gmail.com>
2024-08-16 18:14:37 -0500

    update 1(autopartes test)

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Navegacion from "./plantilla/Navegacion";
 import ListadoServicios from "./servicios/ListadoServicios";
+import AutoPartes from "./autopartes/AutoPartes";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
     <Navegacion/>
     <Routes>
       <Route exact path="/" element={<ListadoServicios/>}/>
+      <Route exact path="/autopartes" element={<AutoPartes/>}/>
     </Routes>
     </BrowserRouter>
     </div>
