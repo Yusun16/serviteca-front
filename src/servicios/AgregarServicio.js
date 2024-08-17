3d0af579439cb1b598da2e56150f21f22fcc346f
Yusun16 <yusunguairabryan@gmail.com>
2024-08-17 00:31:59 -0500

    conexion base de datos (test)

--- src/servicios/AgregarServicio.js
+++ src/servicios/AgregarServicio.js
@@ -1,27 +1,57 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 
 export default function AgregarServicio() {
+    let navegacion=useNavigate();
+
+    const[servicio, setServicios]=useState({
+        codigo:"",
+        descripcion:"",
+        valorServicio:"",
+        año:"",
+        porcentajeOperario:""
+    })
+
+    const{codigo, descripcion, valorServicio, año, porcentajeOperario }= servicio
+
+    const onInputCahnge=(e) => {
+        setServicios({...servicio, [e.target.name]:e.target.value})
+    }
+
+    const onSubmit=async (e)=>{
+        e.preventDefault();
+        const urlBase="http://localhost:8080/serviteca/servicios";
+        await axios.post(urlBase, servicio)
+        navegacion("/")
+
+    }
+
   return (
     <div className='container'>
         <div className='container text-center' style={{margin: "30px"}}>
             <h2>Agregar Servicio</h2>
         </div>
-        <form>
+        <form onSubmit={(e)=> onSubmit(e)}> 
         <div className="mb-3">
-            <label for="descripcion" className="form-label">Descripción</label>
-            <input type="text" className="form-control" id="descripcion" name='descripcion' required={true} />
+            <label htmlFor="codigo" className="form-label">Codigo</label>
+            <input type="text" className="form-control" id="codigo" name='codigo' required={true} value={codigo} onChange={(e)=>onInputCahnge(e)} />
         </div>
         <div className="mb-3">
-            <label for="valor del servicio" className="form-label">Valor Del Servicio</label>
-            <input type="number" step="any" className="form-control" id="valor del servicio" name="valor del servicio"/>
+            <label htmlfor="descripcion" className="form-label">Descripción</label>
+            <input type="text" className="form-control" id="descripcion" name='descripcion' required={true} value={descripcion} onChange={(e)=>onInputCahnge(e)} />
         </div>
         <div className="mb-3">
-            <label for="año" className="form-label">Año</label>
-            <input type="number" step="any" className="form-control" id="año" name='año'/>
+            <label htmlFor="valorServicio" className="form-label">valor Servicio</label>
+            <input type="text" className="form-control" id="valorServicio" name='valorServicio' required={true} value={valorServicio} onChange={(e)=>onInputCahnge(e)} />
         </div>
         <div className="mb-3">
-            <label for="porcenteaje del operario" className="form-label">Porcentaje del Operario</label>
-            <input type="number" step="any" className="form-control" id="porcenteaje del operario" name='porcentaje del operario'/>
+            <label htmlfor="año" className="form-label">Año</label>
+            <input type="number" step="any" className="form-control" id="año" name='año' value={año} onChange={(e)=>onInputCahnge(e)} />
+        </div>
+        <div className="mb-3">
+            <label htmlfor="porcentajeOperario" className="form-label">Porcentaje Operario</label>
+            <input type="text"  className="form-control" id="porcentajeOperario" name='porcentajeOperario' value={porcentajeOperario} onChange={(e)=>onInputCahnge(e)} />
         </div>
 
         <div className='text-center'>
