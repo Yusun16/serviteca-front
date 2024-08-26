c974f47e524023740fa16856e49953b5da35d74d
Manuel282002 <manuel_caviedesco@fet.edu.co>
2024-08-26 10:45:02 -0500

    inicio(sobreponer el nav)

--- src/logins/JefeTaller.js
+++ src/logins/JefeTaller.js
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import "../styles/LoginForm.css";
 import { Link } from "react-router-dom";
 
 const LoginForm = () => {
--- src/servicios/vistaPrincipal.js
+++ src/servicios/vistaPrincipal.js
@@ -1,10 +1,38 @@
 import React from 'react'
 import imgInicio from '../img/imginicio.png';
+import { Link } from 'react-router-dom';
 
 export default function vistaPrincipal() {
     return (
-        <div className="container">    
-              <img src={imgInicio} alt="Logo de inicio" style={{ width: "500px", height: "200px", position: "relative", left: "410px", top: "200px"}} />
+
+
+
+        <div className="container">
+
+
+            <div>
+
+                <button className="btn btn-outline-light text-black" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasRight" aria-controls="offcanvasRight" style={{ position: "relative", left: "105%" }}><i className="fa-solid fa-bars"></i></button>
+
+                <div className="offcanvas offcanvas-end colonav" tabindex="-1" id="offcanvasRight" aria-labelledby="offcanvasRightLabel">
+                    <div className="offcanvas-header">
+                        <button type="button" className="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+                    </div>
+                    <div className="text">
+                        <Link className='servicios text-white' style={{textDecoration: "none"}} to="/"> Servicios</Link>
+                    </div>
+                </div>
+            </div>
+
+
+            <div>
+                <img src={imgInicio} alt="Logo de inicio" style={{ width: "500px", height: "200px", position: "relative", left: "410px", top: "200px" }} />
+            </div>
+
+
         </div>
+
+
+
     )
 }
