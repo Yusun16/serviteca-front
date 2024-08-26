d527f7f652bac9fcf701dd9c860a5bd99f279e92
Yusun16 <yusunguairabryan@gmail.com>
2024-08-26 10:07:55 -0500

    login (solucionado)

--- src/logins/JefeTaller.js
+++ src/logins/JefeTaller.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
-import "../styles/LoginForm.css";
+import "../logins/styles/LoginForm.css";
 import { Link } from "react-router-dom";
+import servitecalogo from "../img/imginicio.png"
 
 const LoginForm = () => {
   const [activeRole, setActiveRole] = useState("Jefe de taller");
@@ -10,14 +11,15 @@ const LoginForm = () => {
   };
 
   return (
+    <div className="body-login">
     <div className="login-container">
       <img
-        src="path_to_logo.png"
+        src={servitecalogo}
         alt="Premium Car Service Logo"
-        className="logo"
+        className="logo-login"
       />
-      <div className="login-card">
-        <h2>
+      <div className="login-card-1">
+        <h2 className="h2-login">
           <i className="fas fa-user-circle"></i> Iniciar sesión
         </h2>
         <div className="role-tabs">
@@ -41,20 +43,21 @@ const LoginForm = () => {
           </button>
         </div>
         <form>
-          <div className="input-group">
-            <i className="fas fa-user"></i>
-            <input type="text" placeholder="Usuario" required />
+          <div className="input-group-login">
+            <i className="fas fa-user i-login"></i>
+            <input className="input-login" type="text" placeholder="Usuario" required />
           </div>
-          <div className="input-group">
-            <i className="fas fa-lock"></i>
-            <input type="password" placeholder="Contraseña" required />
+          <div className="input-group-login">
+            <i className="fas fa-lock i-login"></i>
+            <input className="input-login" type="password" placeholder="Contraseña" required />
           </div>
-          <Link href="#">Olvidé mi contraseña</Link>
+          <Link className="a-login" href="#">Olvidé mi contraseña</Link>
           <button type="submit" className="login-button">
             Ingresar
           </button>
         </form>
       </div>
+    </div>
     </div>
   );
 };
