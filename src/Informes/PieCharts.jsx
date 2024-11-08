3c72b873ddff0a1225ac358de06ef8029a7c3c7f
Yusun16 <yusunguairabryan@gmail.com>
2024-11-08 11:35:29 -0500

    informe en front listo falta exportar pdf excel

--- src/Informes/PieCharts.jsx
+++ src/Informes/PieCharts.jsx
@@ -1,49 +1,48 @@
 import React, { useEffect, useRef } from 'react';
 import { Chart } from 'chart.js/auto';
 
-function PieCharts() {
+function PieCharts({ labels, data }) {
     const chartRef = useRef(null);
     const chartInstance = useRef(null);
 
     useEffect(() => {
         if (chartInstance.current) {
-            chartInstance.current.destroy()
+            chartInstance.current.destroy();
         }
         const myChartRef = chartRef.current.getContext("2d");
 
         chartInstance.current = new Chart(myChartRef, {
             type: "pie",
             data: {
-                labels: [
-                    'Red',
-                    'Blue',
-                    'Yellow'
-                ],
+                labels: labels,
                 datasets: [{
-                    label: 'My First Dataset',
-                    data: [300, 50, 100],
+                    label: 'Valor Total de Servicios',
+                    data: data,
                     backgroundColor: [
                         'rgb(255, 99, 132)',
                         'rgb(54, 162, 235)',
-                        'rgb(255, 205, 86)'
+                        'rgb(255, 205, 86)',
+                        'rgb(75, 192, 192)',
+                        'rgb(153, 102, 255)',
+                        'rgb(255, 159, 64)'
                     ],
                     hoverOffset: 4
                 }]
             },
-        })
+        });
+
         return () => {
             if (chartInstance.current) {
-                chartInstance.current.destroy()
+                chartInstance.current.destroy();
             }
-        }
-    }, []);
-
+        };
+    }, [labels, data]); // Se actualiza cuando cambian las etiquetas o los datos
 
     return (
         <div>
             <canvas ref={chartRef} style={{ width: "300px", height: "200px" }} />
         </div>
-    )
+    );
 }
 
-export default PieCharts
+export default PieCharts;
